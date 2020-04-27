4a267d436711f4bbfd5ad48843593dbaee83b7b0
Sandesh Prabhu <sandeshprabhu.n@gmail.com>
2020-04-27 16:17:19 +0530

    Initial commit to run the code

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ __version__ = '0.0.4'
 here = path.abspath(path.dirname(__file__))
 
 # get the dependencies and installs
-with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
-    all_reqs = f.read().split('\n')
+# with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+#     all_reqs = f.read().split('\n')
 
-install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
-dependency_links = [x.strip().replace('git+', '') for x in all_reqs if x.startswith('git+')]
+# install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
+# dependency_links = [x.strip().replace('git+', '') for x in all_reqs if x.startswith('git+')]
 
 setup(
     name='mlfromscratch',
@@ -23,8 +23,8 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     author='Erik Linder-Noren',
-    install_requires=install_requires,
+    # install_requires=install_requires,
     setup_requires=['numpy>=1.10', 'scipy>=0.17'],
-    dependency_links=dependency_links,
+    # dependency_links=dependency_links,
     author_email='eriklindernoren@gmail.com'
 )
